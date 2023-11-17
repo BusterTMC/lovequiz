1ddacc3d426f96058092949e6b461dc13f4474f6
BusterTMC <>
2023-11-17 15:14:25 +0000

    added the quetsions and the answers

---
+++ script.js
@@ -0,0 +1,93 @@
+const questions = [
+    {
+        question: "Who painted the Mona Lisa??",
+        answers: [
+            { text: "Vincent van Gogh", correct: false},
+            { text: "Papblo Picasso", correct: false },
+            { text: "Leonardo da Vinci", correct: true },
+            { text: "Michelangelo", correct: false },
+        ]
+    },
+    {
+        question: "What is the capital of France?",
+        answers: [
+            { text: "Paris", correct: true },
+            { text: "Monaco", correct: false },
+            { text: "Marseille", correct: false },
+            { text: "Nice", correct: false },
+        ]
+
+    },
+    {
+        question: "What is the tallest mountain in the world?",
+        answers: [
+            { text: "K2", correct: false },
+            { text: "Mount Everest", correct: true },
+            { text: "Makalu", correct: false },
+            { text: "Lhotse", correct: false },
+        ]
+    },
+    {
+        question: "What is the largest planet in our solar system?",
+        answers: [
+            { text: "Saturn", correct: false },
+            { text: "Uranus", correct: false },
+            { text: "Neptune", correct: false },
+            { text: "Jupiter", correct: true },
+        ] 
+    },
+    {
+        question: "Who wrote the play Romeo and Juliet?",
+        answers: [
+            { text: "J.R.R. Tolkien", correct: false },
+            { text: "Charles Dickens", correct: false },
+            { text: "Lev Tolstoj", correct: false },
+            { text: "William Shakespeare", correct: true },
+        ]
+    },
+    {
+        question: "What is the capital city of Australia?",
+        answers: [
+            { text: "Perth", correct: false },
+            { text: "Canberra", correct: true },
+            { text: "Melbourne", correct: false },
+            { text: "Sydney", correct: false },
+        ] 
+    },
+    {
+        question: "What is the primary language spoken in Brazil?",
+        answers: [
+            { text: "French", correct: false },
+            { text: "Spanish", correct: false },
+            { text: "Portuguese", correct: true },
+            { text: "English", correct: false },
+        ]   
+    },
+    {
+        question: "Who is known as the inventor of the lightbulb?",
+        answers: [
+            { text: "Thomas Edison", correct: true },
+            { text: "Nikola Tesla", correct: false },
+            { text: "Alexander Graham Bell", correct: false },
+            { text: "Albert Einstein", correct: false },
+        ] 
+    },
+    {
+        question: "What is the largest ocean on Earth?",
+        answers: [
+            { text: "Indian", correct: false },
+            { text: "Atlantic", correct: false },
+            { text: "Pacific", correct: true },
+            { text: "Arctic", correct: false },
+        ]    
+    },
+    {
+        question: "What is the capital of the United States?",
+        answers: [
+            { text: "Washington, D.C.", correct: false },
+            { text: "New York", correct: true },
+            { text: "Los Angeles", correct: false },
+            { text: "Seattle", correct: false },
+        ]  
+    }
+]
